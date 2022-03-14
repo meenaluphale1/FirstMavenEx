ab293ca3fb6ec8da860d60baf96e3a8d31db84da
MeenalAnand <meenaluphalegit@gmail.com>
2022-03-14 17:16:55 +0530

    second commit
    addded one extra line in TestCalc Class

--- FirstMavenEx/src/main/java/com/lti/demos/TestCalc.java
+++ FirstMavenEx/src/main/java/com/lti/demos/TestCalc.java
@@ -4,6 +4,7 @@ public class TestCalc {
 	
 	public static void main(String[] args) {
 		
+		System.out.println("new line added ");
 	Calculator c= new Calculator();
 	int r=c.add(300, 200);
 	System.out.println(r);
