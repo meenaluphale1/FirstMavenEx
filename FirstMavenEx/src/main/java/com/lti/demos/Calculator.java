612008cfb1ed05d68dfbda8ef70fa282a8645991
MeenalAnand <meenaluphalegit@gmail.com>
2022-03-14 16:59:20 +0530

    my first commit

---
+++ FirstMavenEx/src/main/java/com/lti/demos/Calculator.java
@@ -0,0 +1,12 @@
+package com.lti.demos;
+
+public class Calculator {
+	
+	public int add(int x, int y)
+	{
+		int result=x+y;
+		
+		return result;
+	}
+
+}
---
+++ FirstMavenEx/src/main/java/com/lti/demos/TestCalc.java
@@ -0,0 +1,13 @@
+package com.lti.demos;
+
+public class TestCalc {
+	
+	public static void main(String[] args) {
+		
+	Calculator c= new Calculator();
+	int r=c.add(300, 200);
+	System.out.println(r);
+		
+	}
+
+}
